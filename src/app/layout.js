e41dc24e3400d44dd38ab725ce54ba7960702dad
Kenneth Kiarie <mwangikennethkiarie@gmail.com>
2026-03-27 16:02:10 +0300

    added dark mode

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import Providers from "./Providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,8 +25,11 @@ export default function RootLayout({ children }) {
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
+        <Providers>
         <Header />
-        {children}</body>
+        {children}
+        </Providers>
+        </body>
     </html>
   );
 }
