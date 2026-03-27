1b92178b1c819865ff866d9d38ac7e4b135c6d69
Kenneth Kiarie <mwangikennethkiarie@gmail.com>
2026-03-27 18:31:34 +0300

    fetch data from tmdb

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,30 @@
-import React from 'react'
+import Results from "@/components/Results";
+import React from "react";
 
-export default function Home() {
+const API_KEY = process.env.API_KEY;
+
+export default async function Home({ searchParams }) {
+  const params = await searchParams;
+  const genre = params.genre || "fetchTrending";
+
+  const res = await fetch(
+    `https://api.themoviedb.org/3${
+      genre === "fetchTopRated" ? `/movie/top_rated` : `/trending/all/week`
+    }?api_key=${API_KEY}&language=en-US&page=1`,
+  );
+
+  if (!res.ok) {
+    // throw new Error("failed to fetch data");
+    console.error("Failed to fetch data, status:", res.status);
+  }
+
+  const data = await res.json(); // ← fixed
+  const results = data.results;
+  
+
   return (
-    <div>Home</div>
-  )
+    <div>
+      <Results results={results} />
+    </div>
+  );
 }
